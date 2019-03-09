ebe1b51e3bba12d856e0e93b27506f8910f0cacf
ayush ruia <ayush@thejaingroup.com>
2019-03-09 12:19:53 +0530

    few commits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,9 @@ def current_personnel
   @current_personnel ||= Personnel.find_by_auth_token( cookies[:auth_token]) if cookies[:auth_token]
 end
 
+def handle_unverified_request
+  raise(ActionController::InvalidAuthenticityToken)
+end
+
 
 end
